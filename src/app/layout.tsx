9cb6a15c6e888aa214c123976c9c9eaac2b57ba8
Satveak <satveak.rakesh2022@vitstudent.ac.in>
2024-06-18 00:21:44 +0530

    cm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,21 +18,13 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={cn("relative h-full bg-black ", inter.className)}>
-				<Navbar />
-				{children}
-			</body>
-		</html>
-	);
-  return (
-    <html lang="en">
       <body className={inter.className}>
-        {/* ****navbar**** */}
+       <Navbar />
         <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">{children}</div>
-          {/* ****Footer**** */}
+          {/* ***Footer*** */}
         </main>
       </body>
-    </html>
-  );
+    </html>
+	);
 }
