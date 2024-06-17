11dc87cfdd41d986054392378b238ac8159434c0
Satveak <satveak.rakesh2022@vitstudent.ac.in>
2024-06-17 23:10:09 +0530

    kj

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
 	prefix: "",
 	theme: {
@@ -77,5 +77,3 @@ const config = {
 	},
 	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
-
-export default config
