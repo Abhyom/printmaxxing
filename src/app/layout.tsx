fcf4ee46cb0208a81f10f1d95a158d29ea9e0ff7
Satveak <satveak.rakesh2022@vitstudent.ac.in>
2024-06-18 00:36:25 +0530

    put .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,15 +16,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-      <body className={inter.className}>
-       <Navbar />
+  return (
+    <html lang="en">
+      <body className={cn("relative h-full bg-black ", inter.className)}>
+        <Navbar />
         <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full">{children}</div>
           {/* ***Footer*** */}
         </main>
       </body>
-    </html>
-	);
+    </html>
+  );
 }
